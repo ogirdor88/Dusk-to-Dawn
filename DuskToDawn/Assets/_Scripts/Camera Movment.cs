0fe21c494c05dfaf338225fe0b336671cbdb0269
Rodrigo <rlopezpatino@gmail.com>
2025-01-30 09:29:20 -0800

    player moves

---
+++ DuskToDawn/Assets/_Scripts/Camera Movment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the offset from the camera and the ball
+        offset = player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the camera to follow the player with the offset
+        transform.position = player.transform.position - offset;
+    }
+}
